e921b2ecc9a486228539f24f550b0705e54dd1e9
archanasanker <50758103+archanasanker@users.noreply.github.com>
2024-01-12 20:10:54 +0530

    program_for_circle

---
+++ Circle.java
@@ -0,0 +1,13 @@
+
+public class Circle extends Shape{
+    public static void main(String[] args) {
+    Circle newCir=new Circle();
+    newCir.print();
+    
+   }
+    
+        public void print() {
+        System.out.println("This is Circle");
+        super.print();
+    }
+}
